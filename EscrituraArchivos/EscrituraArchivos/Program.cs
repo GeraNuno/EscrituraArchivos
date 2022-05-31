f7c15eeac3bb978d239f45e59a59436e647e5cd3
GeraNuno <l21212008@tectijuana.edu.mx>
2022-05-31 11:24:57 -0700

    EscrituraArchivis
    Versión de la clase (ejemplo)

---
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace EscrituraArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt",true);
+
+            string[] lines = {
+                "Esta es la información de la primera línea",
+                "Esta es la segunda línea",
+                "Fin del texto"
+            };
+
+            foreach (string line in lines)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();
+
+            Console.WriteLine("Escribiendo en el archivo...");
+            Console.ReadKey();
+        }
+    }
+}
